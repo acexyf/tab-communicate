fe1a87d43cf462753416028a04f8a711c4127013
corner <corner_scofield@163.com>
2020-09-06 22:56:55 +0800

    优化worker

--- public/shared.js
+++ public/shared.js
@@ -1,12 +1,27 @@
-let shareNum = 0
+let musicNum = null
 
-onconnect = function(e) {
+onconnect = function (e) {
+  console.log('onconnect')
   var port = e.ports[0];
   // port.postMessage('Hello World!');
 
-  port.onmessage = function(e){
-    shareNum++
-    port.postMessage('num:'+shareNum)
+  port.onmessage = function (e) {
+    let {
+      type,
+      data
+    } = e.data
+    if (type == 'get') {
+      port.postMessage({
+        type:'get',
+        data: musicNum
+      });
+    } else if(type == 'set') {
+      musicNum = data
+
+    }
+
+
+
   }
 
 
